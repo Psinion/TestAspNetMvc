e5873543c0f569497b61d88bcce031801557c012
Mosalev Daniel <mosalev.daniel@gmail.com>
2023-01-28 15:52:57 +0700

    Add AspNet.Mvc

--- src/TestAspNetMvc/Program.cs
+++ src/TestAspNetMvc/Program.cs
@@ -1,6 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace TestAspNetMvc;
 
-app.MapGet("/", () => "Hello World!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
 
-app.Run();
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(
+                webBuilder => webBuilder.UseStartup<Startup>()
+            );
+    }
+}
---
+++ src/TestAspNetMvc/Startup.cs
@@ -0,0 +1,30 @@
+﻿namespace TestAspNetMvc;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddMvc(options => options.EnableEndpointRouting = false);
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseStatusCodePages();
+
+        app.UseStaticFiles();
+
+        app.UseMvcWithDefaultRoute();
+    } 
+}
